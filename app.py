9b830873f0e088df2b955849887f7d787d50f712
Abhi G <abhijeet@mintix.no>
2024-12-22 22:58:36 +0530

    updating

--- app.py
+++ app.py
@@ -26,10 +26,9 @@ def main():
             st.markdown(prompt)
     
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(model=st.session_state["openai_model"], messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ], stream=True)
+        stream = client.chat.completions.create(model=st.session_state["openai_model"], 
+        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages], 
+        stream=True)
         response = st.write_stream(stream)
     st.session_state.messages.append({
         "role": "assistant",
